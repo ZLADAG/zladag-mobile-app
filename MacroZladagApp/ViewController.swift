f8a0dc13cb765c7d15c05638ee29e020beb7b497
daniel-bss <daniel.simamora2000@gmail.com>
2023-10-02 14:38:10 +0700

    init project

---
+++ MacroZladagApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MacroZladagApp
+//
+//  Created by Daniel Bernard Sahala Simamora on 02/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
