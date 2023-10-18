caf0dc47156dc6ae12c0a3e6eff77b9db6dfe71f
It's me <74585420+cmp9797@users.noreply.github.com>
2023-10-18 12:09:17 +0700

    feat: add button n maps action (direct to web)

--- MacroZladagApp/Controllers/Other/BoardingMapViewController.swift
+++ MacroZladagApp/Controllers/Other/BoardingMapViewController.swift
@@ -16,12 +16,12 @@ struct MapViewAttributes {
 }
 
 class BoardingMapViewController: UIViewController, MKMapViewDelegate {
-
+    
     var mapView : MKMapView!
     
     /// u/ passing data
     var mapViewAttr = MapViewAttributes()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -35,7 +35,7 @@ class BoardingMapViewController: UIViewController, MKMapViewDelegate {
         mapView.delegate = self
     }
     
-
+    
     private func createMapView() {
         mapView = MKMapView()
         mapView.translatesAutoresizingMaskIntoConstraints = false
@@ -49,15 +49,31 @@ class BoardingMapViewController: UIViewController, MKMapViewDelegate {
         let pointLocation:CLLocationCoordinate2D = CLLocationCoordinate2DMake(placeLatValue, placeLongValue)
         
         let region:MKCoordinateRegion = MKCoordinateRegion(center: pointLocation, span: theSpan)
+        
         mapView.setRegion(region, animated: true)
+        mapView.isUserInteractionEnabled = true
         
         let pinLocation : CLLocationCoordinate2D = CLLocationCoordinate2DMake(placeLatValue, placeLongValue)
         
         let objectAnnotation = MKPointAnnotation()
         objectAnnotation.coordinate = pinLocation
         objectAnnotation.title = mapViewAttr.annotationTitle
-        self.mapView.addAnnotation(objectAnnotation)
-                
+
+        self.mapView.addAnnotation(objectAnnotation)        
+        
+        mapView.isZoomEnabled = false
+        mapView.isPitchEnabled = false
+        mapView.isRotateEnabled = false
+        mapView.isScrollEnabled = false
+        mapView.isMultipleTouchEnabled = false
+        
+        // Create a UITapGestureRecognizer
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(mapViewTapped(_:)))
+        
+        // Add the UITapGestureRecognizer to the mapView
+        mapView.addGestureRecognizer(tapGesture)
+        
+        
         view.addSubview(mapView)
         setMapConstraint()
     }
@@ -71,6 +87,57 @@ class BoardingMapViewController: UIViewController, MKMapViewDelegate {
             mapView.heightAnchor.constraint(equalToConstant: 150)
         ])
         print("cek")
+    }
+    
+    
+    private func presentOpenMapUrlAlert(_ title: String, _ message: String) {
+        let alertController = UIAlertController(
+            title: title,
+            message: message,
+            preferredStyle: .alert
+        )
+        
+        // Cancel action
+        let cancelAction = UIAlertAction(
+            title: "Cancel",
+            style: .destructive,
+            handler: nil
+        )
+        alertController.addAction(cancelAction)
+        
+        
+        // OK Acrion
+        let okAction = UIAlertAction(
+            title: "OK",
+            style: .default,
+            handler: { (action) in
+                // Handle the "Open Website" button tap
+                self.openMapUrl(self.mapViewAttr.latitude, self.mapViewAttr.longitude)
+            }
+        )
+        alertController.addAction(okAction)
+        
+        // Present the alert
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    private func openMapUrl(_ latitude: CLLocationDegrees, _ longitude: CLLocationDegrees) {
+        if (UIApplication.shared.canOpenURL(URL(string: "comgooglemaps://")!)) {
+            // Google Maps is installed, open it with the coordinates
+            let _: () = openUrl("comgooglemaps://?q=\(latitude),\(longitude)")
+            
+        } else {
+            // Google Maps is not installed, open it in a web browser
+            let _: () = openUrl("https://www.google.com/maps?q=\(latitude),\(longitude)")
+        }
+    }
+    
+    private func openUrl(_ urlAddress: String) {
+        if let url = URL(string: urlAddress) {
+            if UIApplication.shared.canOpenURL(url) {
+                UIApplication.shared.open(url, options: [:], completionHandler: nil)
+            }
+        }
     }
     
     /// Keep only (don't delete) - func ok but data not accurate
@@ -111,27 +178,31 @@ class BoardingMapViewController: UIViewController, MKMapViewDelegate {
             
             print("placeMark:\(String(describing: placeMark.name))")
         })
-
+        
     }
     
     /// Keep only (don't delete) - func ok but data not accurate
     private func getLocation() {
         let geocoder = CLGeocoder()
-
+        
         geocoder.geocodeAddressString("Apple Developer Academy @BINUS, Sampora, Kabupaten Tangerang, Banten") { (placemarks, error) in
-
-          if error != nil {
-            return
-          } else if let placemarks = placemarks {
-
-            if let coordinate = placemarks.first?.location?.coordinate {
-               //Here's your coordinate
-                self.mapViewAttr.latitude = coordinate.latitude
-                self.mapViewAttr.longitude = coordinate.longitude
+            
+            if error != nil {
+                return
+            } else if let placemarks = placemarks {
+                
+                if let coordinate = placemarks.first?.location?.coordinate {
+                    //Here's your coordinate
+                    self.mapViewAttr.latitude = coordinate.latitude
+                    self.mapViewAttr.longitude = coordinate.longitude
+                }
             }
-          }
         }
     }
     
-
+    // MARK: Selector Functions
+    @objc func mapViewTapped(_ sender: UITapGestureRecognizer) {
+        presentOpenMapUrlAlert("Open Google Maps", "You will be directed to google maps")
+        
+    }
 }
