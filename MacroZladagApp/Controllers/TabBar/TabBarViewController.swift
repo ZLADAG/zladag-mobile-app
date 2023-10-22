cf4929f6bbcf40938462a85c7bbd69f42c1f7b59
daniel-bss <daniel.simamora2000@gmail.com>
2023-10-23 01:24:04 +0700

    tabbar default navbar

--- MacroZladagApp/Controllers/TabBar/TabBarViewController.swift
+++ MacroZladagApp/Controllers/TabBar/TabBarViewController.swift
@@ -18,22 +18,17 @@ class TabBarViewController: UITabBarController {
     
     func configureTabBar() {
         let vc1 = HomeViewController()
-//        let vc1 = SheetDuaViewController()
-//        let vc1 = CobaViewController()
-//        let vc1 = CobaViewController()
         let vc2 = YourOrdersViewController()
-//        let vc2 = CobaTextFieldViewController()
         let vc3 = AccountViewController()
-//        let vc3 = DatePickerViewController()
         
         vc1.title = ""
         vc2.title = "Your Orders"
         vc3.title = "Account"
-        
-        vc1.navigationItem.largeTitleDisplayMode = .always
-        vc2.navigationItem.largeTitleDisplayMode = .always
-        vc3.navigationItem.largeTitleDisplayMode = .always
-        
+//
+//        vc1.navigationItem.largeTitleDisplayMode = .always
+//        vc2.navigationItem.largeTitleDisplayMode = .always
+//        vc3.navigationItem.largeTitleDisplayMode = .always
+//        
         let nav1 = UINavigationController(rootViewController: vc1)
         let nav2 = UINavigationController(rootViewController: vc2)
         let nav3 = UINavigationController(rootViewController: vc3)
@@ -44,8 +39,8 @@ class TabBarViewController: UITabBarController {
         nav3.tabBarItem = UITabBarItem(title: "Account", image: UIImage(named: "tabbar-account-icon"), tag: 3)
         
 //        nav1.navigationBar.prefersLargeTitles = true
-        nav2.navigationBar.prefersLargeTitles = true
-        nav3.navigationBar.prefersLargeTitles = true
+//        nav2.navigationBar.prefersLargeTitles = true
+//        nav3.navigationBar.prefersLargeTitles = true
         
 //        nav1.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default) //UIImage.init(named: "transparent.png")
 //        nav1.navigationController?.navigationBar.shadowImage = UIImage()
